aa5e556c75d54ea632a5981a699d90376d31668a
Tom Goldwing <3741773+tomgoldwing@users.noreply.github.com>
2019-08-03 11:20:19 +0200

    gravity scale

--- Assets/Scripts/dragObject.cs
+++ Assets/Scripts/dragObject.cs
@@ -4,56 +4,65 @@ using UnityEngine;
 
 public class dragObject : MonoBehaviour
 {
-	/*private Vector3 cursorOffset; //diferencia entre la posición del objeto y la del cursor
-
-	//private float posicionZObjeto;
-
-	private void OnMouseDown() //al pulsar el objeto
-	{
-		//posicionZObjeto = Camera.main.WorldToScreenPoint(gameObject.transform.position).z; //leer la coordenada z del objeto
-		
-		cursorOffset = gameObject.transform.position - GetMouseWorldPos(); //Guardar la posición del objeto en el espacio menos la posicion del mouse en el espacio 3d
-	}
-
-	private Vector3 GetMouseWorldPos() //conseguir las coordenadas del ratón en el espacio 3d
-	{
-		//Posición x,y en píxeles del ratón, en dos dimensiones
-		Vector3 mousePos = Input.mousePosition;
-
-		//Usar la coordenada z del objeto
-		//mousePos.z = posicionZObjeto;
-
-		return Camera.main.ScreenToViewportPoint(mousePos);
-	}
-	private void OnMouseDrag()
-	{
-		transform.position = GetMouseWorldPos() + cursorOffset; //mover el objeto a la posicion del ratón sumando cierto offset
-	}*/
-
-	//public GameObject player;
-
-	bool dragging = false;
+	bool dragging;
 	Vector3 mouseStartPos;
 	Vector3 playerStartPos;
 
+	Vector3 newPos;
+
+	bool useGravity;
+
+	private void Start()
+	{
+		dragging = false;
+		useGravity = true;
+	}
+
 	private void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0)) //pulsar click izquierdo
 		{
 			dragging = true;
-			mouseStartPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+			//Posiciones iniciales al empezar a arrastrar
+			mouseStartPos = GetMousePos();
 			playerStartPos = gameObject.transform.position;
 		}
-		else if (Input.GetMouseButtonUp(0))
+		else if (Input.GetMouseButtonUp(0)) //soltar click izquierdo
 		{
 			dragging = false;
+			if (!useGravity)
+			{
+				GetComponent<Rigidbody2D>().gravityScale = 1;
+				useGravity = true;
+			}
 		}
 
 		if (dragging)
 		{
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-			Vector3 move = mousePos - mouseStartPos;
-			gameObject.transform.position = playerStartPos + move;
+			//Posición nueva
+			newPos = GetMousePos() - mouseStartPos;
+		}
+	}
+
+	private Vector3 GetMousePos() //conseguir las coordenadas del ratón 2d del ratón
+	{
+		//Posición x,y en píxeles del ratón, en dos dimensiones
+		Vector3 mousePos = Input.mousePosition;
+
+		return Camera.main.ScreenToWorldPoint(mousePos);
+	}
+
+	private void FixedUpdate()
+	{
+		if (dragging)
+		{
+			if (useGravity)
+			{
+				GetComponent<Rigidbody2D>().gravityScale = 0;
+				useGravity = false;
+			}
+			gameObject.transform.position = playerStartPos + newPos; //mover objeto a nueva posición
 		}
 	}
 }
