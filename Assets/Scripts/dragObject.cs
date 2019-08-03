e25abde45f6cd3d7bf682b48323d4b8181c0bcd1
Tom Goldwing <3741773+tomgoldwing@users.noreply.github.com>
2019-08-03 12:48:37 +0200

    magic area

--- Assets/Scripts/dragObject.cs
+++ Assets/Scripts/dragObject.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class dragObject : MonoBehaviour
 {
+	Rigidbody2D rb2d;
 	bool dragging;
+	bool useGravity;
+	bool enArea;
 	Vector3 mouseStartPos;
 	Vector3 playerStartPos;
-
 	Vector3 newPos;
 
-	bool useGravity;
+
+	private void Awake()
+	{
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
 
 	private void Start()
 	{
@@ -19,30 +25,45 @@ public class dragObject : MonoBehaviour
 	}
 
 	private void Update()
+	{		
+		if (Input.GetMouseButtonUp(0)) //soltar click izquierdo
+		{
+			//Stop Dragging
+			dragging = false;
+			if (!useGravity) useGravity = true;
+		}		
+
+		if (dragging)
+		{
+			//Posición nueva
+			newPos = GetMousePos() - mouseStartPos;
+
+			gameObject.transform.position = playerStartPos + newPos; //mover objeto a nueva posición
+
+			if (Input.GetMouseButtonDown(1)) //pulsar botón secundario
+			{
+				if (rb2d.rotation == 0f)
+				{
+					rb2d.rotation = -90f;
+				}
+				else
+				{
+					rb2d.rotation = 0f;
+				}				
+			}
+		}
+	}
+
+	private void OnMouseDown() //pulsar click izquierdo en el collider
 	{
-		if (Input.GetMouseButtonDown(0)) //pulsar click izquierdo
+		if (enArea)
 		{
 			dragging = true;
 
 			//Posiciones iniciales al empezar a arrastrar
 			mouseStartPos = GetMousePos();
 			playerStartPos = gameObject.transform.position;
-		}
-		else if (Input.GetMouseButtonUp(0)) //soltar click izquierdo
-		{
-			dragging = false;
-			if (!useGravity)
-			{
-				GetComponent<Rigidbody2D>().gravityScale = 1;
-				useGravity = true;
-			}
-		}
-
-		if (dragging)
-		{
-			//Posición nueva
-			newPos = GetMousePos() - mouseStartPos;
-		}
+		}		
 	}
 
 	private Vector3 GetMousePos() //conseguir las coordenadas del ratón 2d del ratón
@@ -57,12 +78,50 @@ public class dragObject : MonoBehaviour
 	{
 		if (dragging)
 		{
-			if (useGravity)
-			{
-				GetComponent<Rigidbody2D>().gravityScale = 0;
-				useGravity = false;
-			}
-			gameObject.transform.position = playerStartPos + newPos; //mover objeto a nueva posición
+			if (useGravity) useGravity = false;
+			rb2d.velocity = Vector3.zero;
+			rb2d.angularVelocity = 0f;
+			//gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+
+			//gameObject.transform.position = playerStartPos + newPos; //mover objeto a nueva posición
+		}
+		if (useGravity && (rb2d.gravityScale != 1))
+		{
+			rb2d.gravityScale = 1;
+		}
+		else if (!useGravity && (rb2d.gravityScale != 0))
+		{
+			rb2d.gravityScale = 0;
 		}
 	}
+
+	private void OnTriggerEnter2D(Collider2D trigger)
+	{
+		if (trigger.gameObject.CompareTag("magic"))
+		{
+			if (!enArea) enArea = true;
+			Debug.Log("area tocada");
+		}
+	}
+	private void OnTriggerExit2D(Collider2D trigger)
+	{
+		if (enArea) enArea = false;
+		//Stop Dragging
+		if (dragging) dragging = false;
+		if (!useGravity) useGravity = true;
+
+		Debug.Log("area left");
+	}
+	
+	/*
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.gameObject.CompareTag("wall"))
+		{
+			//Stop Dragging
+			if (dragging) dragging = false;
+			if (!useGravity) useGravity = true;
+		}
+	}
+	*/
 }
---
+++ Assets/Scripts/magicArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magicArea : MonoBehaviour
+{
+	Vector3 posicionInicial;
+	public GameObject mago;
+
+	private void Awake()
+	{
+		this.posicionInicial = new Vector3(this.gameObject.transform.localPosition.x, this.gameObject.transform.localPosition.y, this.gameObject.transform.localPosition.z);
+	}
+
+	void Update()
+	{
+		this.transform.position = new Vector3(mago.transform.position.x + posicionInicial.x, this.transform.position.y, this.transform.position.z); //el fondo sigue al jugador
+	}
+}
